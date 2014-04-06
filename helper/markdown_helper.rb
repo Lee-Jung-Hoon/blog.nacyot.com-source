45364823861962be22b6bcb1e1caf1d3563dcbe4
Kim Daekwon <propellerheaven@gmail.com>
2014-04-06 15:45:50 +0900

    Update

--- helper/markdown_helper.rb
+++ helper/markdown_helper.rb
@@ -16,4 +16,14 @@ class HTMLwithPygments < Redcarpet::Render::HTML
   def block_code(code, language)
     Pygments.highlight(code, :lexer => language, options: {linespans: 'line'})
   end
+
+  def preprocess(document)
+    document.gsub!(/\[\[ipython:(.*?)\]\]/) do |m|
+      cmd = "ipython nbconvert --to html --template basic ./source/iruby/#{$1}.ipynb --output ./source/iruby/#{$1}"
+      system(cmd)
+      File.read("./source/iruby/#{$1}.html")
+    end
+
+    document
+  end
 end
---
+++ source/articles/2014-04-06-test-ipython.md
@@ -0,0 +1,7 @@
+---
+title: "Iruby Test"
+date: 2014-04-06 10:20:32 +0900
+author: nacyot
+---
+
+[[ipython:euler-032]]
