908abc5d20d0829dedf6ce25ce8ffa16badfff2b
nacyot <propellerheaven@gmail.com>
2015-06-28 18:17:51 +0900

    Update content pages and data and styles

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,7 +2,12 @@
 //= require requirejs/require
 //= require moment/min/moment.min
 //= require bootstrap/dist/js/bootstrap.min
+//= require toc/dist/toc
+//= require d3/d3.min
+//= require lodash/lodash.min
+//= require chance/chance
+//= require hr
 // require angular/angular.min
 // require angular-sanitize/angular-sanitize.min
 // require masonry/dist/masonry.pkgd.min
-//= require toc/dist/toc.min
+
